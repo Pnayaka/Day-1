b51e72e7eace74356298267c751ada2e930555a1
pnayaka <pari2sn@gmail.com>
2020-05-11 20:05:16 +0530

    Updated the Dependency
    Dependency  solution was earlyer made wrong now its perfectly right

---
+++ 1.2/Calculator/src/main/java/org/nayaka/calculator.java
@@ -0,0 +1,12 @@
+package org.nayaka;
+
+public class calculator {
+
+	void add(int a, int b) {
+		System.out.println("Addition:"+(a+b));
+	}
+	
+	void sub(int a, int b) {
+		System.out.println("Subtration:"+(a-b));
+	}
+}
--- 1.3/Dependency/src/main/java/org/nayaka/App.java
+++ 1.3/Dependency/src/main/java/org/nayaka/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        calculator cal = new calculator();
+        cal.add(10, 20);
+        cal.sub(30, 10);
     }
 }
